351db23944752957c15992e7a1950201e89570b3
Spencer Brower <spencer@macbook.local>
2020-09-25 23:04:45 -0400

    fix: Bound hammerspoon keys the way I like 'em.

--- files/hammerspoon/init.lua
+++ files/hammerspoon/init.lua
@@ -1,2 +1,19 @@
 hs.loadSpoon("ShiftIt")
-spoon.ShiftIt:bindHotkeys({})
+spoon.ShiftIt:bindHotkeys({
+    left = {{ 'ctrl', 'alt', 'cmd' }, 'h' },
+    right = {{ 'ctrl', 'alt', 'cmd' }, 'l' },
+    up = {{ 'ctrl', 'alt', 'cmd' }, 'k' }, 
+    down = {{ 'ctrl', 'alt', 'cmd' }, 'j' },
+    upleft = {{ 'ctrl', 'alt', 'cmd' }, 'y' },
+    upright = {{ 'ctrl', 'alt', 'cmd' }, 'u' },
+    botleft = {{ 'ctrl', 'alt', 'cmd' }, 'b' },
+    botright = {{ 'ctrl', 'alt', 'cmd' }, 'n' },
+    maximum = {{ 'ctrl', 'alt', 'cmd' }, 'space' },
+    -- toggleFullScreen = {{ 'ctrl', 'alt', 'cmd' }, 'f' },
+    -- toggleZoom = {{ 'ctrl', 'alt', 'cmd' }, 'z' },
+    -- center = {{ 'ctrl', 'alt', 'cmd' }, 'c' },
+    -- nextScreen = {{ 'ctrl', 'alt', 'cmd' }, 'n' },
+    -- previousScreen = {{ 'ctrl', 'alt', 'cmd' }, 'p' },
+    -- resizeOut = {{ 'ctrl', 'alt', 'cmd' }, '=' },
+    -- resizeIn = {{ 'ctrl', 'alt', 'cmd' }, '-' }
+})
